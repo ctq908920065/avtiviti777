d460c48718e54d6b14c9bd3bfa551a65783fc6bc
ctq <fei957878@gmail.com>
2023-12-27 21:24:10 +0800

    Initial commit

---
+++ src/main/java/com/example/avtiviti777/Avtiviti777Application.java
@@ -0,0 +1,13 @@
+package com.example.avtiviti777;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Avtiviti777Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Avtiviti777Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/avtiviti777/Avtiviti777ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avtiviti777;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Avtiviti777ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
